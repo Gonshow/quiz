1263af127d3c46d7f6c8604846c56c434773c18d
Gonshow <exit.ajisai@gmail.com>
2020-05-06 23:09:08 +0900

    0506

--- app.js
+++ app.js
@@ -6,17 +6,9 @@ var db = new sqlite3.Database('./db/testdb.sqlite3');
 
 
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
+
 /*接続確認用*/
-/*
-app.get('/index', (req, res) => {
-  connection.query(
-    'SELECT * FROM items',
-    (error, results) => {
-      res.render('index.ejs', {items: results});
-    }
-  );
-});
-*/
 app.get('/', (req, res) => {
   db.all(
     'SELECT * FROM Q ORDER BY RANDOM() LIMIT 3',
@@ -28,7 +20,19 @@ app.get('/', (req, res) => {
     }
   );
 });
-
+app.post('/', (req, res) =>{
+  //console.log(req.body.itemName);
+  db.all(
+    'SELECT * FROM Q ORDER BY RANDOM() LIMIT 3',
+    (err, rows) => {
+      res.render('sample.ejs', {ITEMS: rows});
+      rows.forEach(function (row) {
+          console.log(row);
+        });
+    }
+  );
+}
+);
 /*
 db.get('SELECT * FROM Q', function(err, row) {
     if (err) {
@@ -38,20 +42,4 @@ db.get('SELECT * FROM Q', function(err, row) {
 });
 */
 
-/*db.all('SELECT * FROM Q ;', function(err, rows) {
-    if (err) {
-        throw err;
-    }
-    rows.forEach(function (row) {
-        console.log(row);
-      });
-      app.get('/', (req, res) => {
-        res.render('sample.ejs', {
-          TEST: 'test',
-          ITEMS: rows
-        });
-      });
-});
-
-*/
 app.listen(3000);
